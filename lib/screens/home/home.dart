9a34bd3a8861df8abc7286819df95e6cb9844725
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-03-12 13:34:53 +0300

    update homepage

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:course_app/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,6 +7,27 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kBackround,
+        elevation: 0,
+        centerTitle: false,
+        title: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Text(
+            "Hello Gülsen!",
+            style: TextStyle(fontSize: 16, color: kFontLight),
+          ),
+        ),
+        actions: [
+          Stack(
+            children: [
+              Container(
+                child: Icon(Icons.notifications, size: 30,),
+              )
+            ],
+          )
+        ],
+      ),
       body: Center(
         child: Text("Home Page"),
       ),
