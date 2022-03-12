e283ec309acde38c8a42c6261b6c395a9aa0182f
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-03-12 19:32:20 +0300

    notification icon update

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -7,34 +7,52 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kBackround,
-        elevation: 0,
-        centerTitle: false,
-        title: Padding(
-          padding: const EdgeInsets.only(left: 10),
-          child: Text(
-            "Hello Gülsen!",
-            style: TextStyle(fontSize: 16, color: kFontLight),
-          ),
-        ),
-        actions: [
-          Stack(
-            children: [
-              Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(color: kFontLight, width: 2)),
-                  child: Image.asset(
-                    'assets/icons/notification.png',
-                    width: 30,
-                  ))
-            ],
-          )
-        ],
-      ),
+      appBar: _buildAppBar(),
       body: Center(
         child: Text("Home Page"),
       ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: kBackround,
+      elevation: 0,
+      centerTitle: false,
+      title: Padding(
+        padding: const EdgeInsets.only(left: 10),
+        child: Text(
+          "Hello Gülsen!",
+          style: TextStyle(fontSize: 16, color: kFontLight),
+        ),
+      ),
+      actions: [
+        Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 10, right: 20),
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  border:
+                      Border.all(color: kFontLight.withOpacity(0.3), width: 2),
+                  borderRadius: BorderRadius.circular(15)),
+              child: Image.asset(
+                'assets/icons/notification.png',
+                width: 20,
+              ),
+            ),
+            Positioned(
+              top: 15,
+              right: 30,
+              child: Container(
+                height: 8,
+                width: 8,
+                decoration: BoxDecoration(color: kAccent, shape: BoxShape.circle),
+              ),
+            )
+          ],
+        )
+      ],
     );
   }
 }
