855f95050cfa3ad21cb7cf167655ced1324139a4
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-03-12 12:54:55 +0300

    added home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:course_app/screens/home/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: HomePage(),
+    );
   }
 }
