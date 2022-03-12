d2f88ddd326b1dde970fa0408d2189d547aa6040
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-03-12 13:05:38 +0300

    SystemChrome.setSystemUIOverlayStyle updated
       //todo bu yukardaki kalan şarj saat vs gibi simgeleri kaldırmayın sağlar
        SystemChrome.setSystemUIOverlayStyle(
            SystemUiOverlayStyle(statusBarColor: Colors.transparent));

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:course_app/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //todo bu yukardaki kalan şarj saat vs gibi simgeleri kaldırmayın sağlar
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
