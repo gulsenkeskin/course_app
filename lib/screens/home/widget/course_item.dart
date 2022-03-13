489ed93cc74a459d243f86ba1da136ce601cc8cf
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-03-13 15:15:58 +0300

    Update course_item.dart

--- lib/screens/home/widget/course_item.dart
+++ lib/screens/home/widget/course_item.dart
@@ -1,3 +1,4 @@
+import 'package:course_app/constants/colors.dart';
 import 'package:course_app/models/course.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +10,73 @@ class CourseItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Course'),
+      child: Stack(
+        children: [
+          Container(
+            height: 250,
+            width: 250,
+            decoration: BoxDecoration(
+              color: kPrimaryLight,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                Expanded(
+                    flex: 2,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                        topLeft: Radius.circular(20),
+                      ),
+                      child: Image.asset(
+                        course.imageUrl,
+                        fit: BoxFit.cover,
+                      ),
+                    )),
+                Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Image.asset(
+                                course.authorImg,
+                                width: 20,
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                course.author,
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text(course.title),
+                              //todo nokta :D hahah
+                              Container(
+                                margin: EdgeInsets.symmetric(horizontal: 5),
+                                width: 5,
+                                height: 5,
+                                decoration: BoxDecoration(
+                                    color: kFontLight, shape: BoxShape.circle),
+                              ),
+                              Text('2h 22min')
+                            ],
+                          )
+                        ],
+                      ),
+                    ))
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
