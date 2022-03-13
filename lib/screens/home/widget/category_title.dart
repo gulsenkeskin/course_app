e702301feacc998076c5cd9ad1c72e53b8fb0bb2
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-03-13 11:35:44 +0300

    Update category_title.dart

--- lib/screens/home/widget/category_title.dart
+++ lib/screens/home/widget/category_title.dart
@@ -1,19 +1,30 @@
+import 'package:course_app/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class CategoryTitle extends StatelessWidget {
- final String leftText;
- final String rightText;
+  final String leftText;
+  final String rightText;
 
- const CategoryTitle({Key? key, required this.leftText, required this.rightText}) : super(key: key);
-
+  const CategoryTitle(
+      {Key? key, required this.leftText, required this.rightText})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(horizontal: 25),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(leftText),
-          Text(rightText),
+          Text(
+            leftText,
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: kFont),
+          ),
+          Text(
+            rightText,
+            style: TextStyle(fontSize: 16, color: kFontLight),
+          ),
         ],
       ),
     );
